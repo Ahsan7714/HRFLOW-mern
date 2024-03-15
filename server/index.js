8ca70aab754616d8619654fd1dcf2fb6cf530c00
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2024-03-15 06:14:46 +0500

    .

--- server/index.js
+++ server/index.js
@@ -7,39 +7,33 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const path = require('path');
 
-
-
 connectDB();
 
+// Serve static files from the client/dist directory
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-      
-      origin: ["http://localhost:5173"],
-      
-      credentials: true,
+        origin: ["http://localhost:5173"],
+        credentials: true,
     })
-  );
+);
 
 const admin = require('./routes/adminRoutes')
-app.use('/api/v1',admin)
+app.use('/api/v1', admin);
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
-
-
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "../client/dist/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
+// Catch-all route to serve index.html
+app.get('*', function (_, res) {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'), function (err) {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
 });
 
-
-app.listen(port,()=>{
-    console.log(`server is running on localhost ${port}`)
-})
+app.listen(port, () => {
+    console.log(`server is running on localhost ${port}`);
+});
