74542843e4b0355fb229aece3a4c91fd51394c95
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2024-03-15 08:18:50 +0500

    .

--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    // origin: ["http://localhost:5173"],
+    origin:["https://hrflow.vercel.app/"],
     credentials: true,
   })
   );
