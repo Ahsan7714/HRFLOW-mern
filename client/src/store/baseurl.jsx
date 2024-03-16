65dfdf7693f0e85c90d3f6764239489fab1af0f6
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2024-03-16 11:22:57 +0500

    .

--- client/src/store/baseurl.jsx
+++ client/src/store/baseurl.jsx
@@ -1,4 +1,4 @@
 // const baseurl = "http://localhost:8000/api/v1"
-const baseurl = "https://hrflow-mern-backend-five.vercel.app/"
+const baseurl = "https://hrflow-mern-backend-five.vercel.app/api/v1"
 
 export default baseurl
